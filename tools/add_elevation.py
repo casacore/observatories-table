42b043597dabde67ba1bdc8a9dab77e6ed28c0bf
Ole Streicher <olebole@debian.org>
2016-11-01 11:40:19 +0100

    Initial json table and tools

---
+++ tools/add_elevation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import urllib.request
+
+# The following function was used to retrieve the elevation.
+# Get the API key from https://maps.googleapis.com/
+def get_elevation(longitude, latitude, api_key):
+    url="https://maps.googleapis.com/maps/api/elevation/json?" + \
+    "locations={longitude},{latitude}&key={key}"
+    response = urllib.request.urlopen(url.format(longitude=longitude,
+                                                 latitude=latitude,
+                                                 key=api_key))
+    data = json.loads(str(response.read().decode()))
+    return int(data['results'][0]['elevation'])
+
+default_file = "observatories.json"
+
+parser = argparse.ArgumentParser(
+    usage = 'casacore-update-observatories [options]',
+    description = 'Update casacore Observatory table from json input')
+parser.add_argument('-f', '--file',
+                    help='JSON input file (default: {})'
+                    .format(default_file),
+                    default=default_file)
+parser.add_argument('-k', '--api-key',
+                    help='Google maps API key. Get it from https://developers.google.com/maps/documentation/elevation/start')
+args = parser.parse_args()
+
+if not args.api_key:
+    print('API key required. Get it from https://maps.googleapis.com')
+    exit(1)
+    
+with open(args.file) as f:
+    obstable = json.load(f)
+
+found = False
+for row in obstable:
+    if 'elevation' not in row:
+        found = True
+        row['elevation'] = get_elevation(row['longitude'], row['latitude'],
+                                         args.api_key)
+
+if found:
+    with open(args.file + '.new', 'w') as f:
+        json.dump(obstable, f, indent=4, sort_keys=True)
+
+    if not os.path.exists(args.file + '.old'):
+        os.rename(args.file, args.file + '.old')
+    os.rename(args.file + '.new', args.file)
---
+++ tools/create_casacore_table.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import dateutil.parser
+import json
+
+import pyproj
+from casacore import tables
+
+default_version = "1.1"
+default_date = "2016-11-01T11:42"
+
+default_input = "observatories.json"
+default_path = "Observatories"
+
+parser = argparse.ArgumentParser(
+    usage = 'casacore-update-observatories [options]',
+    description = 'Update casacore Observatory table from json input')
+parser.add_argument('-i', '--input-file',
+                    help='JSON oinput file (default: {})'
+                    .format(default_input),
+                    default=default_input)
+parser.add_argument('-o', '--output-path',
+                    help='output table path (default: {})'
+                    .format(default_path),
+                    default=default_path)
+parser.add_argument('-v', '--version',
+                    help='Set version number in table (default: {})'
+                    .format(default_version),
+                    default=default_version)
+parser.add_argument('-d', '--date',
+                    help='Set version date in database (default: {})'
+                    .format(default_date),
+                    default=default_date)
+args = parser.parse_args()
+
+with open(args.input_file) as f:
+    obstable = json.load(f)
+
+# Create data table
+columns = [
+    tables.makescacoldesc('MJD', 0., valuetype='double',
+                          keywords={'UNIT':'d'}),
+    tables.makescacoldesc('Name', '', valuetype='string'),
+    tables.makescacoldesc('Type', '', valuetype='string'),
+    tables.makescacoldesc('Long', 0., valuetype='double',
+                          keywords={'UNIT':'deg'}),
+    tables.makescacoldesc('Lat', 0., valuetype='double',
+                          keywords={'UNIT':'deg'}),
+    tables.makescacoldesc('Height', 0., valuetype='double',
+                          keywords={'UNIT':'m'}),
+    tables.makescacoldesc('X', 0., valuetype='double',
+                          keywords={'UNIT':'m'}),
+    tables.makescacoldesc('Y', 0., valuetype='double',
+                          keywords={'UNIT':'m'}),
+    tables.makescacoldesc('Z', 0., valuetype='double',
+                          keywords={'UNIT':'m'}),
+    tables.makescacoldesc('Source', '', valuetype='string'),
+    tables.makescacoldesc('Comment', '', valuetype='string'),
+]
+
+t = tables.table(args.output_path, tables.tablecreatedesc(columns),
+                 len(obstable))
+t.putinfo({'type': 'IERS', 'subType': 'observatory'})
+t.putkeywords({
+    'MJD0': 0,
+    'dMJD': 0.0,
+    'VS_VERSION': '{:04d}.{:04d}'.format(*(int(i)
+                                           for i in args.version.split('.'))),
+    'VS_CREATE': dateutil.parser.parse(args.date).strftime('%Y/%m/%d/%H:%M'),
+    'VS_DATE': dateutil.parser.parse(args.date).strftime('%Y/%m/%d/%H:%M'),
+    'VS_TYPE': 'List of Observatory positions'
+})
+
+ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+
+tablerows = t.row()
+for i, row in enumerate(obstable):
+    x, y, z = pyproj.transform(lla, ecef, row['longitude'],
+                               row['latitude'], row['elevation'])
+    tablerows.put(i, {
+        'MJD': 0.0,
+        'Name': row['Id'],
+        'Type': 'WGS84',
+        'Long': row['longitude'],
+        'Lat':  row['latitude'],
+        'Height':  row['elevation'],
+        'X': x,
+        'Y': y,
+        'Z': z,
+        'Source': row.get('Reference', 'Wikipedia')
+    })
